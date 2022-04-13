bbd23ff0ecb32e308ac0ec73a8295211b53ca4ed
ruslanpylypenko <ruslan.pylypenko@socialboost.com.ua>
2022-04-13 22:21:36 +0300

    validate status field

--- src/validators/noteSchema.ts
+++ src/validators/noteSchema.ts
@@ -1,8 +1,9 @@
 import { body } from 'express-validator'
-import { CATEGORIES } from '../helpers/constants'
+import { CATEGORIES, STATUS } from '../helpers/constants'
 
 export const noteSchema = [
   body('name').notEmpty().withMessage('Name is required'),
   body('category').isIn(CATEGORIES).withMessage('Category is not correct'),
   body('content').notEmpty().withMessage('Content is required'),
+  body('status').isIn([null,STATUS.ACTIVE, STATUS.ARCHIVED]),
 ]
